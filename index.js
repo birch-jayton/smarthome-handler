979cd06ae87139a5428868b918112532f7a1d76b
Jayton Birch <jayton.birch@familysearch.org>
2021-03-30 21:15:36 -0600

    sleep and turn off funcitonality

--- index.js
+++ index.js
@@ -2,13 +2,49 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const port = process.env.PORT || 80
-// parse various different custom JSON types as JSON
+
 app.use(bodyParser.json())
 
-app.post('/motion-detected', (req, res) => {
+// CONFIGS
+const sleepTimeMs = 180000
+
+
+//event names for ifttt webhooks
+const PORCH_LIGHT_ON_EVENT = "turn_porch_light_on"
+const PORCH_LIGHT_OFF_EVENT = "turn_porch_light_off"
+
+function touchWebhookWithEvent(event){
+    const iftttWebhookUrl = `https://maker.ifttt.com/trigger/${event}/with/key/${process.env.IFTTT_WEBHOOK_KEY}`
+    axios.get(iftttWebhookUrl)
+            .then(res => res.ok)
+            .catch(e => console.log(e))
+}
+
+//smart object
+const PORCH_LIGHT = {
+    turnOn: () => touchWebhookWithEvent(PORCH_LIGHT_ON_EVENT),
+    turnOff: () => touchWebhookWithEvent(PORCH_LIGHT_OFF_EVENT)
+}
+
+function sleep (ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+app.post('/motion-detected', async (req, res) => {
     console.log(req.body)
-    res.sendStatus(200)
+    if(req.body.auth === process.env.SECRET_KEY){
+        res.sendStatus(200)
+        //turn lights on
+        PORCH_LIGHT.turnOn()
+        await sleep(sleepTimeMs)
+        PORCH_LIGHT.turnOff()
+
+        // wait then turn off
+    } else {
+        res.sendStatus(403)
+    }
 })
 
 app.listen(port, () => {
